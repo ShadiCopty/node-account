d5ba428026fecca711c93aa38900cf8f685c9738
Shadi Copty <shadi.copty@yahoo.com>
2016-12-22 18:27:34 -0800

    Second commit.

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+
+    'secret': 'ilovescotchyscotch',
+    'database': 'mongodb://localhost/so9pojyN'
+
+};
+
+//'database': 'mongodb://accountUser:password&54;127.0.0.1:27017/so9pojyN'
